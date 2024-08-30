0eb97b5c77d24d4f761face9ed239dd95f23bb9a
Artur Shiriev <lesnik512@gmail.com>
2024-08-30 08:56:20 +0300

    integrate advanced alchemy (#7)

--- app/exceptions.py
+++ app/exceptions.py
@@ -1,27 +1,16 @@
 import typing
 
 import litestar
+from advanced_alchemy.exceptions import ForeignKeyError
 from litestar import status_codes
 
 
-class DatabaseError(Exception):
-    pass
-
-
-class DatabaseValidationError(DatabaseError):
-    def __init__(self, message: str, field: str | None = None) -> None:
-        self.message = message
-        self.field = field
-
-
-def database_validation_exception_handler(
-    _: object, exc: DatabaseValidationError
-) -> litestar.Response[dict[str, typing.Any]]:
+def database_validation_exception_handler(_: object, exc: ForeignKeyError) -> litestar.Response[dict[str, typing.Any]]:
     return litestar.Response(
         media_type=litestar.MediaType.JSON,
         content={
             "detail": "Database validation failed",
-            "extra": [{"message": exc.message, "key": exc.field or "__root__"}],
+            "extra": [{"message": exc.detail, "key": "__root__"}],
         },
         status_code=status_codes.HTTP_400_BAD_REQUEST,
     )
