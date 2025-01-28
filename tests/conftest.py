fdf7219f84dadad2d172262a7a6a9f11d47377fb
Artur Shiriev <lesnik512@gmail.com>
2025-01-28 10:01:44 +0300

    update packages

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,6 @@
-import logging
 import typing
 
+import modern_di
 import modern_di_litestar
 import pytest
 from httpx import ASGITransport, AsyncClient
@@ -8,9 +8,6 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 from app import ioc
 from app.application import application
-
-
-logger = logging.getLogger(__name__)
 
 
 @pytest.fixture
@@ -22,20 +19,25 @@ async def client() -> typing.AsyncIterator[AsyncClient]:
         yield client
 
 
-@pytest.fixture(autouse=True)
-async def db_session() -> typing.AsyncIterator[AsyncSession]:
-    async with modern_di_litestar.fetch_di_container(application) as di_container:
-        engine = await ioc.Dependencies.database_engine.async_resolve(di_container)
-        connection = await engine.connect()
-        transaction = await connection.begin()
-        await connection.begin_nested()
-        ioc.Dependencies.database_engine.override(connection, di_container)
+@pytest.fixture
+async def di_container() -> typing.AsyncIterator[modern_di.Container]:
+    di_container_: typing.Final = modern_di_litestar.fetch_di_container(application)
+    async with di_container_:
+        yield di_container_
 
-        try:
-            yield AsyncSession(connection, expire_on_commit=False, autoflush=False)
-        finally:
-            if connection.in_transaction():
-                await transaction.rollback()
-            await connection.close()
-            await engine.dispose()
-            logger.info("Fixture db_session is closed")
+
+@pytest.fixture(autouse=True)
+async def db_session(di_container: modern_di.Container) -> typing.AsyncIterator[AsyncSession]:
+    engine = await ioc.Dependencies.database_engine.async_resolve(di_container)
+    connection = await engine.connect()
+    transaction = await connection.begin()
+    await connection.begin_nested()
+    ioc.Dependencies.database_engine.override(connection, di_container)
+
+    try:
+        yield AsyncSession(connection, expire_on_commit=False, autoflush=False)
+    finally:
+        if connection.in_transaction():
+            await transaction.rollback()
+        await connection.close()
+        await engine.dispose()
