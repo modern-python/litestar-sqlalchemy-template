47088ebdc1e2322afb158a819f92ffff56830128
Artur Shiriev <lesnik512@gmail.com>
2025-04-19 12:39:37 +0300

    update modern-di

--- app/application.py
+++ app/application.py
@@ -1,6 +1,3 @@
-import contextlib
-import typing
-
 import litestar
 import modern_di_litestar
 from advanced_alchemy.exceptions import DuplicateKeyError
@@ -12,34 +9,25 @@ from app.api.decks import ROUTER
 from app.settings import settings
 
 
-class AppBuilder:
-    def __init__(self) -> None:
-        self.app: litestar.Litestar = litestar.Litestar(
-            debug=settings.debug,
-            lifespan=[self.lifespan_manager],
-            exception_handlers={
-                DuplicateKeyError: exceptions.duplicate_key_error_handler,
-            },
-            route_handlers=[ROUTER],
-            dependencies={
-                **modern_di_litestar.prepare_di_dependencies(),
-                "decks_service": modern_di_litestar.FromDI(ioc.Dependencies.decks_service),
-                "cards_service": modern_di_litestar.FromDI(ioc.Dependencies.cards_service),
-            },
-            openapi_config=OpenAPIConfig(
-                title="Litestar Example",
-                description="Example of Litestar with Scalar OpenAPI docs",
-                version="0.0.1",
-                render_plugins=[SwaggerRenderPlugin()],
-            ),
-        )
-        self.di_container = modern_di_litestar.setup_di(self.app)
-
-    @contextlib.asynccontextmanager
-    async def lifespan_manager(self, _: litestar.Litestar | None) -> typing.AsyncIterator[None]:
-        async with self.di_container:
-            await ioc.Dependencies.async_resolve_creators(self.di_container)
-            yield
+def build_app() -> litestar.Litestar:
+    return litestar.Litestar(
+        debug=settings.debug,
+        exception_handlers={
+            DuplicateKeyError: exceptions.duplicate_key_error_handler,
+        },
+        route_handlers=[ROUTER],
+        plugins=[modern_di_litestar.ModernDIPlugin()],
+        dependencies={
+            "decks_service": modern_di_litestar.FromDI(ioc.Dependencies.decks_service),
+            "cards_service": modern_di_litestar.FromDI(ioc.Dependencies.cards_service),
+        },
+        openapi_config=OpenAPIConfig(
+            title="Litestar Example",
+            description="Example of Litestar with Scalar OpenAPI docs",
+            version="0.0.1",
+            render_plugins=[SwaggerRenderPlugin()],
+        ),
+    )
 
 
-application = AppBuilder().app
+application = build_app()
